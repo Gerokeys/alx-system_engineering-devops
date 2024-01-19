0361cb0d8e84d1147da9f4471f9662369c123390
Gerokeys <shikukugerald111@gmail.com>
2024-01-19 12:53:36 -0800

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
+# installs flask
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
