19d2badb3ddd3b083122e3b48b56b80b9c5dcba1
Gerokeys <shikukugerald111@gmail.com>
2024-03-06 11:29:14 -0800

    This is the first commit

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# automates apache2 server
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin/'
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Readme file
+This is the readme file for the 0x17-web_stack_debugging_3
